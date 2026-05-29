9b68edca7008d1dd9058603ce02f75a60ea3e84a
alex <andy.vmoreira@gmail.com>
2026-05-29 21:47:44 +0200

    todo

--- projects/00-hola-mundo/src/main.jsx
+++ projects/00-hola-mundo/src/main.jsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+
+root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(
+ <App />  
+) 
+
+/*
+const Button = ({ text }) => {
+  return (
+    <button>
+      {text}
+    </button> 
+  )
+}
+
+root.render(
+  <React.Fragment>
+    // los nombnres de los componentes deben empezar con mayuscula se usa PascalCase
+    <Button text="Click me!" />
+
+  </React.Fragment>,
 )
+*/
